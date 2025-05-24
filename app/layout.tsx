0a18f18eb1d833c5ebeb0dabc721072f23c3a89d
Tanusha <tanushachoudhary18@gmail.com>
2025-05-24 22:57:23 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const workSans=localFont(options:{
+  src:[
+    {
+      path:'./fonts/WorkSans-Black.ttf',
+      weight:'900',
+      style:'normal',
+    }
+  ]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
